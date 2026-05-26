afe4e7c6f7c9656e60690a042bff317411188261
Agustín García <agustinmanuelgg@protonmail.com>
2026-05-26 10:33:17 -0600

    Lección 2 terminada: Estructuras condicionales if y else

---
+++ Condición.py
@@ -0,0 +1,6 @@
+entrada = input("Por favor, introdusce tu edad: ")
+edad = int(entrada)
+if edad >= 18:
+    print("🔓 Acceso CONCEDIDO. Bienvenido al laboratorio.")
+else:
+    print("🔒 Acceso DENEGADO. Debes de ser mayor de edad para ingresar.")
---
+++ Variables.py
@@ -0,0 +1,4 @@
+nombre_usurio = input("¿Cómo te llamas?")
+proyectos_completados = 1
+print("¡Hola, " + nombre_usurio + "!")
+print("Actualmente tienes " + str(proyectos_completados) + " proyectos en tu portafolio.")
